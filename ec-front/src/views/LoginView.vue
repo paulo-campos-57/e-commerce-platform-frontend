9ac9fafa3a1f682605335642f65a056d7f44d637
paulo-campos-57 <paulo.m.campos6601@gmail.com>
2026-05-23 21:43:49 -0300

    feat: connect with backend

--- ec-front/src/views/LoginView.vue
+++ ec-front/src/views/LoginView.vue
@@ -2,16 +2,23 @@
   <div class="login-container">
     <h1 class="login-title">Bem-vindo de volta!</h1>
 
-    <form class="login-form" @submit.prevent="handleSubmit">
+    <form class="login-form" @submit.prevent="handleLogin">
+      <p
+        v-if="errorMessage"
+        style="color: var(--color-danger); text-align: center; margin-bottom: 1rem"
+      >
+        {{ errorMessage }}
+      </p>
       <div class="form-group">
         <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input v-model="formData.email" type="email" id="email" name="email" required />
       </div>
 
       <div class="form-group">
         <label for="password">Senha:</label>
         <div class="password-wrapper">
           <input
+            v-model="formData.password"
             :type="isPasswordVisible ? 'text' : 'password'"
             id="password"
             name="password"
@@ -28,22 +35,54 @@
         </div>
       </div>
 
-      <button class="login-button" type="submit">Entrar</button>
+      <button class="login-button" type="submit" :disabled="isLoading">
+        {{ isLoading ? "Entrando..." : "Entrar" }}
+      </button>
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+import { useRouter } from "vue-router";
+import { userService } from "@/services/userService";
+import api from "@/services/api";
 
+const router = useRouter();
 const isPasswordVisible = ref(false);
+const isLoading = ref(false);
+const errorMessage = ref("");
+
+const formData = reactive({
+  email: "",
+  password: "",
+});
 
 const togglePasswordVisibility = () => {
   isPasswordVisible.value = !isPasswordVisible.value;
 };
 
-const handleSubmit = () => {
-  console.log("Formulário enviado!");
+const handleLogin = async () => {
+  isLoading.value = true;
+  errorMessage.value = "";
+
+  try {
+    const data = await userService.login(formData);
+
+    localStorage.setItem("token", data.access_token);
+    localStorage.setItem("user", JSON.stringify(data.user));
+
+    console.log("Nest answer:", data);
+    alert(data.message);
+
+    router.push("/home");
+  } catch (error) {
+    console.error("Erro na requisição:", error);
+    errorMessage.value =
+      error.response?.data?.message || "Ocorreu um erro ao fazer login.";
+  } finally {
+    isLoading.value = false;
+  }
 };
 </script>
 
